fbf2695d161bc3d394bde6abedd7690638455aa6
kimabraham <kimkr88@icloud.com>
2024-03-09 06:32:16 +0900

    Feat: setting project router

--- routes/projects.js
+++ routes/projects.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const {
+  getProjects,
+  createProject,
+  deleteProject,
+  modifyProject,
+  updateProject,
+  exportHtml,
+  exportGoogle,
+  getProject,
+} = require("../controllers/project.controller");
+const { verifyObjectId } = require("../middlewares/verifyObjectId");
 const router = express.Router();
 
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
+router.get("/", getProjects);
+router.post("/", createProject);
+router.delete("/:id", verifyObjectId, deleteProject);
+router.put("/:id", verifyObjectId, modifyProject);
+router.patch("/:id", verifyObjectId, updateProject);
+router.get("/:id", verifyObjectId, getProject);
+router.post("/html", exportHtml);
+router.post("/google", exportGoogle);
 
 module.exports = router;
