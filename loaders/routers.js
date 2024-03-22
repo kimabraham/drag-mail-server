8584fa1d17f640af21d5a7f8ae259f36742f6979
kimabraham <kimkr88@icloud.com>
2024-03-23 01:19:50 +0900

    Feat: create node router and controller2

--- loaders/routers.js
+++ loaders/routers.js
@@ -1,11 +1,13 @@
 const users = require("../routes/users");
 const auth = require("../routes/auth");
 const projects = require("../routes/projects");
+const nodes = require("../routes/nodes");
 const contacts = require("../routes/contacts");
 
 const routerLoader = async (app) => {
   app.use("/api/auth", auth);
   app.use("/api/projects", projects);
+  app.use("/api/nodes", nodes);
   app.use("/api/users", users);
   app.use("/api/contacts", contacts);
 };
